3ba239d959df49acd76e66eb997692ab3f541da5
mtp1999 <mohamad.tavakolpoorsaleh@gmail.com>
2023-10-19 18:49:51 +0330

    django advanced alibigdeli season6 part3
    Description:
    using django rest_framework viewsets.ViewSet
    
    for:
    1-post list
    2-create post
    3-update post
    4-post detail

--- backend/appBlog/api/v1/urls.py
+++ backend/appBlog/api/v1/urls.py
@@ -6,13 +6,17 @@ from django.conf.urls.static import static
 app_name = 'appBlog-api-v1'
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='post_list'),
-    path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+    # path('posts/', views.PostList.as_view(), name='post_list'),
+    # path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+
     # path('blog/category/<str:category>/', views.BlogView.as_view(), name='blog_category'),
     # path('blog/tag/<str:tag>/', views.BlogView.as_view(), name='blog_tag'),
     # path('blog/author/<str:author>/', views.BlogView.as_view(), name='blog_author'),
     # path('blog/<int:pid>/', views.SingleView.as_view(), name='single'),
 
+    path('posts/', views.PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post_list'),
+    path('posts/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
+                                                       'delete': 'destroy'}), name='post_detail'),
 ]
 
 if settings.DEBUG:
--- backend/appBlog/api/v1/views.py
+++ backend/appBlog/api/v1/views.py
@@ -1,17 +1,54 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from appBlog.models import Post
 from .serializers import PostSerializer
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import viewsets
+from rest_framework.response import Response
 
 
-class PostList(ListCreateAPIView):
-    serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)
+class PostViewSet(viewsets.ViewSet):
+
     permission_classes = [IsAuthenticated]
-
-
-class PostDetail(RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)
-    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        queryset = Post.objects.filter(status=True)
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = Post.objects.filter(status=True)
+        post = get_object_or_404(queryset, id=pk)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=self.request.POST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        queryset = Post.objects.filter(status=True)
+        post = get_object_or_404(queryset, id=pk)
+        serializer = self.serializer_class(post, data=self.request.POST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        queryset = Post.objects.filter(status=True)
+        post = get_object_or_404(queryset, id=pk)
+        serializer = self.serializer_class(post, data=self.request.POST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        queryset = Post.objects.filter(status=True)
+        post = get_object_or_404(queryset, id=pk)
+        post.delete()
+        return Response({'Detail': 'post deleted.'})
+
+
 
