835839f7348e8d62dda55872d0d9b925a457d5b1
mtp1999 <mohamad.tavakolpoorsaleh@gmail.com>
2023-10-20 21:10:41 +0330

    django advanced alibigdeli season6 part4
    Description:
    1-using from rest_framework import viewsets.ModelViewSet
    in appBlog api-v1 posts for CRUD
    2-using default router for urls.

--- backend/appBlog/api/v1/urls.py
+++ backend/appBlog/api/v1/urls.py
@@ -2,23 +2,28 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
 
 app_name = 'appBlog-api-v1'
 
-urlpatterns = [
-    # path('posts/', views.PostList.as_view(), name='post_list'),
-    # path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+# urlpatterns = [
+#     path('posts/', views.PostList.as_view(), name='post_list'),
+#     path('posts/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
+#
+#     path('blog/category/<str:category>/', views.BlogView.as_view(), name='blog_category'),
+#     path('blog/tag/<str:tag>/', views.BlogView.as_view(), name='blog_tag'),
+#     path('blog/author/<str:author>/', views.BlogView.as_view(), name='blog_author'),
+#     path('blog/<int:pid>/', views.SingleView.as_view(), name='single'),
+#
+#     path('posts/', views.PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post_list'),
+#     path('posts/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
+#                                                        'delete': 'destroy'}), name='post_detail'),
+# ]
+#
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-    # path('blog/category/<str:category>/', views.BlogView.as_view(), name='blog_category'),
-    # path('blog/tag/<str:tag>/', views.BlogView.as_view(), name='blog_tag'),
-    # path('blog/author/<str:author>/', views.BlogView.as_view(), name='blog_author'),
-    # path('blog/<int:pid>/', views.SingleView.as_view(), name='single'),
-
-    path('posts/', views.PostViewSet.as_view({'get': 'list', 'post': 'create'}), name='post_list'),
-    path('posts/<int:pk>/', views.PostViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update',
-                                                       'delete': 'destroy'}), name='post_detail'),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+router = routers.DefaultRouter()
+router.register(r'posts', views.PostViewSet, basename='posts')
+urlpatterns = router.urls
--- backend/appBlog/api/v1/views.py
+++ backend/appBlog/api/v1/views.py
@@ -1,54 +1,16 @@
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from appBlog.models import Post
 from .serializers import PostSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 
 
-class PostViewSet(viewsets.ViewSet):
-
-    permission_classes = [IsAuthenticated]
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
 
-    def list(self, request):
-        queryset = Post.objects.filter(status=True)
-        serializer = self.serializer_class(queryset, many=True)
-        return Response(serializer.data)
-
-    def retrieve(self, request, pk=None):
-        queryset = Post.objects.filter(status=True)
-        post = get_object_or_404(queryset, id=pk)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = self.serializer_class(data=self.request.POST)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        queryset = Post.objects.filter(status=True)
-        post = get_object_or_404(queryset, id=pk)
-        serializer = self.serializer_class(post, data=self.request.POST)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def partial_update(self, request, pk=None):
-        queryset = Post.objects.filter(status=True)
-        post = get_object_or_404(queryset, id=pk)
-        serializer = self.serializer_class(post, data=self.request.POST)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-    def destroy(self, request, pk=None):
-        queryset = Post.objects.filter(status=True)
-        post = get_object_or_404(queryset, id=pk)
-        post.delete()
-        return Response({'Detail': 'post deleted.'})
 
 
 
